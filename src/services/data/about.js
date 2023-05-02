f9deb4acebf9a6efab4a226fbd9f8371fd625f85
Gabriel Grubba <Grubba27@hotmail.com>
2023-05-01 21:45:08 -0300

    Revert "Revert "updated site""
    This reverts commit b510982433b0814289c05a9a730def0cb2142653.

--- src/services/data/about.js
+++ src/services/data/about.js
@@ -5,7 +5,7 @@ export const about = {
   content: {
     paragraphs: [
       "Hello there! My name is Gabriel Grubba and I'm a full time SWE working with OSS now for Meteor.js as a Maintainer",
-      'I have a passion for open source and I love to share my knowledge with the community. My main stack is the Web, specifically JavaScript.',
+      'I have a passion for open source and I love to share my knowledge with the community. My main stack is the Web, specifically JavaScript/TypeScript. I also have experience with Go and C#.',
       'I am currently doing a degree in Computer Science. I have a passion for learning new things and I am always looking to be in the bleeding edge.',
     ],
   },
