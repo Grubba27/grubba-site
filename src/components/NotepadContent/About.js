d10d7cd9c761a4709be0315d58f0ee0c29181235
Gabriel Grubba <Grubba27@hotmail.com>
2023-05-01 21:35:19 -0300

    added sponsor button

--- src/components/NotepadContent/About.js
+++ src/components/NotepadContent/About.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React from "react";
+
+function SponsorButton() {
+  return (
+    <iframe
+      src="https://github.com/sponsors/Grubba27/button"
+      title="Sponsor Grubba27"
+      height="32"
+      width="114"
+      style="border: 0; border-radius: 6px;"
+    ></iframe>
+  );
+}
 
 function About({ content }) {
   const { paragraphs } = content;
   return (
     <div>
-      <h2>About</h2>
-      {
-        paragraphs.map((p, i) => (
-          <p key={i}>{p}</p>
-        ))
-      }
-
+      <h2>About</h2> - <SponsorButton />
+      {paragraphs.map((p, i) => (
+        <p key={i}>{p}</p>
+      ))}
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
