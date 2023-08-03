e9dbb21909f19c8a85396228e7ebac440f9ce412
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-08-03 15:11:51 -0300

    updated

--- src/services/data/about.js
+++ src/services/data/about.js
@@ -1,13 +1,14 @@
 export const about = {
-  id: 'about',
-  name: 'About.txt',
-  icon: 'info_bubble',
+  id: "about",
+  name: "About.txt",
+  icon: "info_bubble",
   content: {
     paragraphs: [
       "Hello there! My name is Gabriel Grubba and I'm a full time SWE working with OSS now for Meteor.js as a Maintainer",
-      'I have a passion for open source and I love to share my knowledge with the community. My main stack is the Web, specifically JavaScript/TypeScript. I also have experience with Go and C#.',
-      'I am currently doing a degree in Computer Science. I have a passion for learning new things and I am always looking to be in the bleeding edge.',
-      'About me outside of work, I love card games(last LoR season I was top 100 for a week in NA server), traveling and watching animes with my beatiful girlfriend.',
+      "I have a passion for open source and I love to share my knowledge with the community. My main stack is the Web, specifically JavaScript/TypeScript. I also have experience with Go and C#.",
+      "In 2023 I've won the first place MongoDB hackaton using Golang and Charm",
+      "I am currently doing a degree in Computer Science. I have a passion for learning new things and I am always looking to be in the bleeding edge.",
+      "About me outside of work, I love card games(last LoR season I was top 100 for a week in NA server), traveling and watching animes with my beatiful girlfriend.",
     ],
   },
-}
+};
