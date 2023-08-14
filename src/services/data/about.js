00e07ca23e9b832feead594224389f16433f7d6a
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-08-14 09:58:56 -0300

    adjusted misspeling

--- src/services/data/about.js
+++ src/services/data/about.js
@@ -7,7 +7,7 @@ export const about = {
       "Hello there! My name is Gabriel Grubba and I'm a full time SWE working with OSS now for Meteor.js as a Maintainer",
       "I have a passion for open source and I love to share my knowledge with the community. I have experience with Go and C#.",
       "In 2023 I've won the first place MongoDB hackaton using Golang and Charm",
-      "About me outside of work, I love card games(last LoR season I was top 100 for a week in NA server), traveling and watching animes with my beatiful girlfriend.",
+      "About me outside of work, I love card games(last LoR season I was top 100 for a week in NA server), traveling and watching animes with my beautiful girlfriend.",
     ],
   },
 };
